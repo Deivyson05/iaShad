41fff3432a8202c854ce92b2ade903aa1b7ee730
Deivyson05 <deivysonrssantos@outlook.com>
2026-09-17 16:51:30 -0300

    chore: adiciona embedding

--- ia-api/src/rag/embedding/embedding.service.ts
+++ ia-api/src/rag/embedding/embedding.service.ts
@@ -1,4 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class EmbeddingService {}
+export class EmbeddingService {
+    constructor(
+        private readonly http: HttpService
+    ){}
+
+    async generate(text: string): Promise<number[]> {
+        const { data } = await firstValueFrom(
+            this.http.post(
+                `${process.env.GEMMA3_URL}/api/embed/`,
+                {
+                    model: 'nomic-embed-text',
+                    input: text
+                }
+            )
+        );
+        return data.embeddings[0];
+    }
+}
