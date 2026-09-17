f3b8fd49c665b6307ff826ad0e960d8db5010ea3
Deivyson05 <deivysonrssantos@outlook.com>
2026-09-17 16:49:35 -0300

    chore: adiciona chunking

--- ia-api/src/rag/chunking/chunking.service.ts
+++ ia-api/src/rag/chunking/chunking.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ChunkingService {}
+export class ChunkingService {
+    chunk(
+        text: string,
+        chunkSize: 500,
+        overlap = 100
+    ): string[] {
+        const chunks: string[] = [];
+        let start = 0;
+        
+        while( start < text.length ) {
+            const end = start + chunkSize;
+
+            chunks.push(
+                text.slice(start, end),
+            );
+            start += chunkSize - overlap;
+        }
+
+        return chunks;
+    }
+}
